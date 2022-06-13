26edb477e1c2608c9f19032bc6fe264fcf9f8d7f
Daenam <daenamdaher@gmail.com>
2022-06-13 19:01:01 -0300

    Recovery pronto

--- recover/recover.c
+++ recover/recover.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-// 0xff 0xd8 0xff 0xe
+#include <cs50.h>
+#include <stdbool.h>
 
-int main(int argc, char *argv[])
+
+// 0xff 0xd8 0xff 0xe
+#define BLOCK_SIZE 512
+
+bool isJpegheader(uint8_t buffer[])
+{
+  return buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0;
+}
+
+int main(int argc, char* argv[])
 {
   if (argc !=2)
   {
@@ -11,14 +21,43 @@ int main(int argc, char *argv[])
       return 1;
   }
 
-  FILE *file = fopen(argv[1], "r");
-  if (file == NULL)
+  FILE *inputPtr = fopen(argv[1], "r");
+  if (inputPtr == NULL)
   {
       printf("Error ocurred while opening the raw file.\n");
       return 1;
   }
 
+  char filename[8];// xxx.jpg'\0'
+  FILE * outputPtr = NULL;
+  uint8_t buffer[BLOCK_SIZE];
+  int jpg_counter = 0;
 
+  while (fread(buffer, sizeof(uint8_t), BLOCK_SIZE, inputPtr) || feof(inputPtr)==0)
+  {
+    if(isJpegheader(buffer))
+    {
+        if (outputPtr!=NULL)
+        {
+          fclose(outputPtr);
+        }
+        sprintf(filename, "%03i.jpg", jpg_counter);
+        outputPtr = fopen(filename, "w");
+        jpg_counter++;
+    }
+    if (outputPtr!= NULL)
+    {
+      fwrite(buffer, sizeof(buffer), 1, outputPtr);
+    }
+  }
+  if (inputPtr==NULL)
+  {
+    fclose(inputPtr);
+  }
 
+  if (outputPtr==NULL)
+  {
+    fclose(outputPtr);
+  }
 
 }
